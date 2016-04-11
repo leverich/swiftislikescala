ca82ad27c8b11668800d591dcfdba04a5d56cc75
Pascal Batty <pascal.batty@soat.fr>
2016-04-11 17:01:06 +0200

    Swift : removed deprecated ++ operator Added `default:` case to non-exhaustive switch statement in pattern matching example

--- comparisons/classes/pattern_match/swift.scala
+++ comparisons/classes/pattern_match/swift.scala
@@ -2,12 +2,14 @@ var movieCount = 0
 var songCount = 0
 
 for item in library {
-  switch item {
+    switch item {
     case let movie as Movie:
-      ++movieCount
-      print("Movie: '\(movie.name)', dir. \(movie.director)")
+        movieCount += 1
+        print("Movie: '\(movie.name)', dir. \(movie.director)")
     case let song as Song:
-      ++songCount
-      print("Song: '\(song.title)'")
-  }
+        songCount += 1
+        print("Song: '\(song.title)'")
+    default:
+        break;
+    }
 }
